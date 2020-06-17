fd474b4deccb3aba7a7164da24c36873834fa24b
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-06-16 21:09:25 -0400

    Done.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
+# RSpec.describe Student, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
 describe Student do
   before(:each) do
     @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
